426800bcc5d2f02c0651e555c9af0dcbc925882b
MJackson <jackswalter7@gmail.com>
2020-09-08 19:30:29 +0200

    Ready for heroku

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mongoose =require('mongoose')
 mongoose.connect('mongodb://localhost//restApi')
 const db = mongoose.connection
 db.on('error',(err) => console.error(err))
+
 db.once('open', () => console.log('connected to database'))
 app.listen(300, () =>{
     console.log('Server started')
