feedcf8d4587e07040631b800e58e5522f284a0d
MJackson <jackswalter7@gmail.com>
2020-09-08 19:44:24 +0200

    Adding procfile

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express')
 var app = express()
 var mongoose =require('mongoose')
+var port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost//restApi')
-const db = mongoose.connection
-db.on('error',(err) => console.error(err))
+//mongoose.connect('mongodb://localhost//restApi')
+//const db = mongoose.connection
+//db.on('error',(err) => console.error(err))
 
-db.once('open', () => console.log('connected to database'))
-app.listen(300, () =>{
+//db.once('open', () => console.log('connected to database'))
+app.get('/', (req, res) =>{
+    res.send('Welcome...');
+})
+app.listen( port, () =>{
     console.log('Server started')
 })
