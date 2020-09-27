da586222e75d4768f2b81c906466380374550af4
MJackson <jackswalter7@gmail.com>
2020-09-27 02:55:37 +0200

    fixing port for heroku

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ getToken(app)
 
 //wellcome route
 app.get('/', (req, res) =>{
-    res.send('Wellcome to Mjackson\'s restful Api (github)')
+    res.send('Welcome to Mjackson\'s restful Api (github)')
 })
 
 // ########## CONTACTS ##########
